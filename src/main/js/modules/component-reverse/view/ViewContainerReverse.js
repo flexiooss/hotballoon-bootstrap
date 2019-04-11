f9021e548ff52a8e44b16b3e156f9532e4aae5d0
Bruno FL <follet-bruno@hotmail.fr>
2019-04-11 14:51:12 +0200

    Je capture l'evenement venant de la vue Puis je crée une action que j'envoie via l'action
    L'application est fonctionnelle

--- src/main/js/modules/component-reverse/view/ViewContainerReverse.js
+++ src/main/js/modules/component-reverse/view/ViewContainerReverse.js
@@ -1,5 +1,5 @@
-import { ViewContainer, ViewContainerParameters } from 'hotballoon'
-import { ViewReverse } from './views/ViewReverse'
+import { EventListenerOrderedBuilder, ViewContainer, ViewContainerParameters } from 'hotballoon'
+import { UPDATE_EVENT, ViewReverse } from './views/ViewReverse'
 import { FLEXIO_IMPORT_OBJECT } from 'flexio-jshelpers'
 
 const ActionUpdatePayloadBuilder = window[FLEXIO_IMPORT_OBJECT].io.flexio.component_reverse.actions.ActionUpdatePayloadBuilder
@@ -13,9 +13,23 @@ export class ViewContainerReverse extends ViewContainer {
     this.__storeReversePublic = storeReversePublic
     this.__action = action
     this.__createView()
+    this.__handleEventFromView()
   }
 
   __createView() {
     this.view = this.addView(new ViewReverse(this, this.__storeReversePublic))
+  }
+
+  __handleEventFromView() {
+    this.view._on(
+      EventListenerOrderedBuilder
+        .listen(UPDATE_EVENT)
+        .callback((payload) => {
+          let actionBuilder = new ActionUpdatePayloadBuilder()
+          let action = actionBuilder.label(payload).build()
+          this.__action.dispatch(action)
+        })
+        .build()
+    )
   }
 }
